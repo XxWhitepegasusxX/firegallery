359a9b9d0b7182944dfeb9864c9951c3e8981c6e
Alexander Almeida <alexanderalmeida3040@gmail.com>
2022-06-24 15:59:59 -0300

    first commit

---
+++ src/firebase/config.js
@@ -0,0 +1,14 @@
+import * as firebase from "firebase/app";
+import 'firebase/storage'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAU1ubCzIotT4vd4VGgCGdM3xW0JAuV-O8",
+    authDomain: "firegallery-5e7b7.firebaseapp.com",
+    projectId: "firegallery-5e7b7",
+    storageBucket: "firegallery-5e7b7.appspot.com",
+    messagingSenderId: "266197312176",
+    appId: "1:266197312176:web:dc7eb5fcf39722299d5175"
+};
+  
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
