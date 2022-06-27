99f8508ffb2111f10483708461d214ef9ebaa190
Alexander Almeida <alexanderalmeida3040@gmail.com>
2022-06-27 16:30:02 -0300

    images rendering

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -12,8 +12,11 @@ const firebaseConfig = {
 };
   
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+{!firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();}
+
 const projectStorage = firebase.storage();
 const projectFirestore = firebase.firestore();
+const timestamp = firebase.firestore.FieldValue.serverTimestamp
 
-export { projectStorage, projectFirestore };
+
+export { projectStorage, projectFirestore, timestamp };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import ImageGrid from '../components/imageGrid';
+import Modal from '../components/Modal';
 import Title from '../components/Title';
 import UploadForm from '../components/UploadForm';
+import { useState } from 'react';
+
 
 export default function Home() {
+  const [selectedImg, setSelectedImg] = useState(null)
+
   return (
     <div className='App'>
       <Title/>
       <UploadForm/>
+      <ImageGrid setSelectedImg={setSelectedImg}/>
+      {selectedImg && <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} />}
     </div>
   )
 }
